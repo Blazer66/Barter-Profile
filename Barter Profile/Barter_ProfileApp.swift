3d94e389ae86da058ea04ab1c2102a24d549bde8
Chrisite L Richardson <crichardson@catholiccharities.net>
2021-04-29 15:48:17 -0500

    Initial Commit

---
+++ Barter Profile/Barter_ProfileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Barter_ProfileApp.swift
+//  Barter Profile
+//
+//  Created by admin on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct Barter_ProfileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
